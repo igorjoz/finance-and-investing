10aff866f6f77e4818dfb68606122fb79008a2e1
Igor Józefowicz <igorjozefowicz@gmail.com>
2022-12-30 23:53:15 +0100

    add Image & User controllers ideas

--- src/app/Controllers/ImageController.php
+++ src/app/Controllers/ImageController.php
@@ -69,3 +69,125 @@ class ImageController
         }
     }
 }
+
+
+
+
+// <?php
+// require_once '../models/Img.php';
+// require_once '../views/LayoutView.php';
+// require_once '../views/RedirectView.php';
+// require_once '../views/JsonView.php';
+// require_once '../image_helpers.php';
+
+// class ImgController {
+//   public function new() {
+//     return new LayoutView('imgnew');
+//   }
+
+//   public function add() {
+//     $user = currentUser();
+//     $valid = true;
+
+//     if ($user) {
+//       $author = $user->name;
+//     } else {
+//       $author = post('author');
+//       if ($author == '') {
+//         $valid = false;
+//         Flash::error("Author can't be empty");
+//       }
+//     }
+
+//     $title = post('title');
+//     if ($title == '') {
+//       $valid = false;
+//       Flash::error("Title can't be empty");
+//     }
+
+//     $watermark = post('watermark');
+//     if ($watermark == '') {
+//       $valid = false;
+//       Flash::error("Watermark can't be empty");
+//     }
+
+//     $access = post('access');
+//     if ($access == '') {
+//       if ($user) {
+//         $valid = false;
+//       } else {
+//         $public = true;
+//       }
+//     } else {
+//       $public = $access == 'public';
+//     }
+
+//     if (isset($_FILES['img']) && $_FILES['img']['size'] > 0) {
+//       $filePath = $_FILES['img']['tmp_name'];
+//       $type = mime_content_type($filePath);
+//       $format = explode('/', $type)[1];
+//       if ($format !== 'jpeg' && $format !== 'png') {
+//         $valid = false;
+//         Flash::error("\"{$type}\" is not an acceptable file type");
+//       }
+
+//       $fileSize = $_FILES['img']['size'];
+//       if ($fileSize > 1024 * 1024) {
+//         $valid = false;
+//         Flash::error("The image is too big (max 1 MB)");
+//       }
+//     } else {
+//       $valid = false;
+//       Flash::error("File can't be empty");
+//     }
+
+//     if ($valid) {
+//       $img = new Img($author, $title, $public, $format);
+//       $img->save();
+//       $id = $img->id();
+//       $name = "{$id}.{$format}";
+//       $path = getcwd() . "/images/{$name}";
+//       if (!rename($filePath, $path)) {
+//         Flash::error("Couldn't save file");
+//       }
+
+//       $thumbnailPath = getcwd() . "/thumb/{$id}.png";
+//       generateThumbnail($path, $thumbnailPath, $format);
+
+//       $watermarkPath = getcwd() . "/preview/{$id}.png";
+//       generateWatermark($path, $watermarkPath, $format, $watermark);
+
+//       Flash::info('Image added');
+//     }
+
+//     return new RedirectView('/img/new', 303);
+//   }
+
+//   public function index() {
+//     $user = currentUser();
+//     $imgs = Img::getAll();
+//     $imgs = array_filter($imgs, function ($img) use ($user) {
+//       return $img->public || ($user && $img->author == $user->name);
+//     });
+//     return new LayoutView('imglist', ['imgs' => $imgs]);
+//   }
+
+//   public function search() {
+//     return new LayoutView('search');
+//   }
+
+//   public function get() {
+//     $title = $_GET['title'];
+//     $imgs = Img::getAll([
+//       'title' => new MongoDB\BSON\Regex($title, 'i')
+//     ]);
+//     $user = currentUser();
+//     $imgs = array_values(array_filter($imgs, function ($img) use ($user) {
+//       return $img->public || ($user && $user->name == $img->author);
+//     }));
+//     $ids = array_map(function ($img) {
+//       return $img->id();
+//     }, $imgs);
+//     return new JsonView($ids);
+//   }
+// }
--- src/app/Controllers/UserController.php
+++ src/app/Controllers/UserController.php
@@ -56,3 +56,88 @@ class UserController
         exit;
     }
 }
+
+
+
+
+
+
+// <?php
+// require_once '../models/User.php';
+// require_once '../views/LayoutView.php';
+// require_once '../views/RedirectView.php';
+
+// class UserController {
+//   public function signup() {
+//     if (currentUser()) {
+//       return new RedirectView('/', 303);
+//     } else {
+//       return new LayoutView('signupform');
+//     }
+//   }
+
+//   public function add() {
+//     $valid = true;
+
+//     $name = post('name');
+//     if ($name == '') {
+//       $valid = false;
+//       Flash::error("Name can't be empty");
+//     }
+
+//     $email = post('email');
+//     if ($email == '') {
+//       $valid = false;
+//       Flash::error("Email can't be empty");
+//     }
+
+//     $password = post('password');
+//     if ($password == '') {
+//       $valid = false;
+//       Flash::error("Password can't be empty");
+//     }
+
+//     $password_r = post('password_r');
+//     if ($password_r != $password) {
+//       $valid = false;
+//       Flash::error("Passwords have to be the same");
+//     }
+
+//     $user = User::get(['name' => $name]);
+//     if ($user) {
+//       $valid = false;
+//       Flash::error("That name is already taken");
+//     }
+
+//     if ($valid) {
+//       $user = new User($name, $email, password_hash($password, PASSWORD_DEFAULT));
+//       $user->save();
+//       Flash::info("You can now log in");
+//       return new RedirectView('/login', 303);
+//     } else {
+//       return new RedirectView('/signup', 303);
+//     }
+//   }
+
+//   public function login() {
+//     if (currentUser()) {
+//       return new RedirectView('/', 303);
+//     } else {
+//       return new LayoutView('loginform');
+//     }
+//   }
+
+//   public function authenticate() {
+//     $name = post('name');
+//     $password = post('password');
+//     $user = User::get(['name' => $name]);
+//     if (!$user || !$user->validPassword($password)) {
+//       Flash::error("Wrong credentials");
+//       return new RedirectView('/login', 303);
+//     } else {
+//       $_SESSION['user'] = $name;
+//       Flash::info("Welcome! You're now logged in.");
+//       return new RedirectView('/', 303);
+//     }
+//   }
+// }
